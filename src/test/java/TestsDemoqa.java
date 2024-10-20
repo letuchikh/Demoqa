3f08a5314bf1c52610b189cdd5ccacbd5e6abe2d
Vladimir <letuchikh@yandex.ru>
2024-10-20 09:18:25 +0300

    Update tests

--- src/test/java/TestsDemoqa.java
+++ src/test/java/TestsDemoqa.java
@@ -9,12 +9,10 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class TestsDemoqa {
     @BeforeAll
-    static void beforeAll() {
+    static void setup() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = false;
-        Configuration.timeout = 5000;
     }
 
     @Test
